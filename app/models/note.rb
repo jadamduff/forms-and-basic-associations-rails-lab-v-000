e8388bd736378540b767ec5924c75219cfdbb202
Adam Duff <j.adam.duff@gmail.com>
2018-09-24 23:06:43 +0000

    automatically backed up by learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,11 @@
 class Note < ActiveRecord::Base
   belongs_to :song
+
+  def note_contents
+    note_contents = []
+    Note.all.each do |note|
+      note_contents << note.content
+    end
+    note_contents
+  end
 end
