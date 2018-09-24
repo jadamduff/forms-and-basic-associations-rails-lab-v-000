95283d9b88c79995b75fcc05c85efaa23aa8611c
Adam Duff <j.adam.duff@gmail.com>
2018-09-24 23:04:11 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,4 +14,8 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
+
+  def genre_name
+    self.artist ? self.artist.name : nil
+  end
 end
