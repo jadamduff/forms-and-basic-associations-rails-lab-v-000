c00f5db6bad8af251aaefdccb70b16813be50afb
Adam Duff <j.adam.duff@gmail.com>
2018-09-24 23:03:52 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,11 +3,15 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def genre_name=(genre)
-    self.genre = Genre.find_or_create_by(name: genre.name)
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
   end
 
   def genre_name
     self.genre ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
