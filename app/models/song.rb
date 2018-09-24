e7453930f8c8e584570dfb9c51a4e5e1e79a0c23
Adam Duff <j.adam.duff@gmail.com>
2018-09-24 23:01:06 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name=(genre)
+    genre = Genre.find_or_create_by(name: genre.name)
+  end
 end
